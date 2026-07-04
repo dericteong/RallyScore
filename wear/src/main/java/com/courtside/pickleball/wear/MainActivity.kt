39291982d5653a4b5f6164997ae91b34dd5dba8c
dericteong <dericteong@gmail.com>
2026-07-04 20:23:14 +1000

    Update MainActivity.kt

--- wear/src/main/java/com/courtside/pickleball/wear/MainActivity.kt
+++ wear/src/main/java/com/courtside/pickleball/wear/MainActivity.kt
@@ -4,21 +4,32 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.fragment.app.FragmentActivity
-import androidx.wear.ambient.AmbientModeSupport
 
 /** Wear host activity for RallyScore watch experiences. */
-class MainActivity : FragmentActivity(), AmbientModeSupport.AmbientCallbackProvider {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WearPhoneSync.initialize(applicationContext)
         WearTabletFallbackSync.initialize(applicationContext)
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        AmbientModeSupport.attach(this)
+        keepScreenOn()
         setContent {
             WearScoreboardApp()
         }
     }
 
-    override fun getAmbientCallback(): AmbientModeSupport.AmbientCallback =
-        object : AmbientModeSupport.AmbientCallback() {}
+    override fun onResume() {
+        super.onResume()
+        keepScreenOn()
+    }
+
+    override fun onPause() {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        window.decorView.keepScreenOn = false
+        super.onPause()
+    }
+
+    private fun keepScreenOn() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        window.decorView.keepScreenOn = true
+    }
 }
